e92da5b076708505f83d3cc6b35de05190f42834
Nelson Rossi Bittencourt <70649161+NelsonBittencourt@users.noreply.github.com>
2021-05-06 09:53:04 -0300

    Version 0.12
    Alteração no exemplo 2 para utilizar o parâmetro de distância da barra de cores.

--- exemplos.py
+++ exemplos.py
@@ -8,7 +8,7 @@ exemplos.py - Rotina para testar o módulo 'plotMap'
               um mapa com modelo semelhante ao utilizado pelo ONS.
 
 Autor   : Nelson Rossi Bittencourt
-Versão  : 0.11
+Versão  : 0.12
 Licença : MIT
 Dependências: numpy, struct, plotMap
 ******************************************************************************
@@ -35,8 +35,7 @@ def main():
                     break                
                 chuva.append(struct.unpack('f',byte))                   
     except:
-        print('Erro ao tentar abrir/acessar arquivo: {}'.format('ETA_24_000.bin'))
-        raise SystemExit
+        raise NameError('Erro ao tentar abrir/acessar arquivo: {}'.format('ETA_24_000.bin'))       
   
 
     # listas com as coordenads (longitudes e latitudes) do arquivo 'Grads'.
@@ -63,6 +62,7 @@ def main():
 
     # Na versão 0.11 foi introduzida a classe 'ArquivoShape', para permitir que o 'shapeFile' seja reaproveitado
     # com mínimo overhead. Você ainda pode utilizar como argumento da função 'plotarMapa' uma string,
+    
     meuArquivoShape = plotMap.ArquivoShape(shapeFile)
     
     # Exemplo 1: Usando o modelo de mapa de forma integral.
@@ -81,7 +81,8 @@ def main():
                         dados=chuva,
                         modeloMapa=mapTemplate,                       
                         destino='exemplo1_mapaTipoONS.jpg',
-                        shapeFile=meuArquivoShape)
+                        shapeFile=meuArquivoShape
+                        )
 
 
     # Exemplo 2: Usando o modelo de mapa de forma parcial.
@@ -97,7 +98,8 @@ def main():
     # Com o objeto 'Mapa', podemos alterar quaisquer dos parâmetros do objeto.
     meuMapaCustomizado.barraCores_titulo = "barra de cores alterada!"
     meuMapaCustomizado.barraCores_orientacao = 'vertical'
-    meuMapaCustomizado.barraCores_posicao = 'left' 
+    meuMapaCustomizado.barraCores_posicao = 'right' 
+    meuMapaCustomizado.barraCores_dist = 0.05
 
     plotMap.plotarMapa(titulo_mapa, lons,lats,chuva,meuMapaCustomizado, 'exemplo2_MapaCustomizado.jpg',meuArquivoShape)
 
@@ -112,16 +114,17 @@ def main():
     plotMap.plotarMapa(titulo_mapa, lons,lats,chuva,meuMapaCustomizado, shapeFile=meuArquivoShape)
 
 
-    # Exemplo 4: Repetindo o exemplo 2, com mapa do tipo 'anomalias' do site WXMaps
-    # O template foi introduzido na versão 0.11
-    # Neste exemplo, não será utilizado 'shapefile'
+    # Exemplo 4: Repetindo o exemplo 2, com mapa do tipo 'anomalias' do site WXMaps.
+    # O template foi introduzido na versão 0.11.
+    # Neste exemplo, não será utilizado 'shapefile'.
+   
     plotMap.plotarMapa(
                         titulo='Anomalia (mm)\nWXMaps',
                         lons=lons,
                         lats=lats,
                         dados=chuva,
                         modeloMapa='templates/AnomaliaWxmaps.dat',
-                        destino='exemplo4_MapaTipoWXMaps_Anomalia1.jpg'
+                        destino='exemplo4_MapaTipoWXMaps_Anomalia.jpg'
                        )    
 
 
