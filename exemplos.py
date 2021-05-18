0775a940aa1e3ddc1e45e6d0a8de3b1fe219198f
Nelson Rossi Bittencourt <70649161+NelsonBittencourt@users.noreply.github.com>
2021-05-18 18:38:29 -0300

    Version 0.121
    Versão 0.121 do arquivo de exemplos.

--- exemplos.py
+++ exemplos.py
@@ -2,13 +2,13 @@
 
 """
 ******************************************************************************
-exemplos.py - Rotina para testar o módulo 'plotMap'
-              Nestes teste será lido um arquivo binário no formato 'Grads' com 
-              dados de chuva prevista. Os dados deste arquivo serão plotados em
-              um mapa com modelo semelhante ao utilizado pelo ONS.
+Rotina para testar o módulo 'plotMap'
+Nestes teste será lido um arquivo binário no formato 'Grads' com dados de chuva
+prevista. Os dados deste arquivo serão plotados em um mapa com modelo semelhante
+ao utilizado pelo ONS.
 
 Autor   : Nelson Rossi Bittencourt
-Versão  : 0.12
+Versão  : 0.121
 Licença : MIT
 Dependências: numpy, struct, plotMap
 ******************************************************************************
@@ -48,78 +48,67 @@ def main():
     # e latitudes. Veja o método 'reshape' do 'numpy' para maiores detalhes.
     chuva = np.array(chuva,dtype=float)    
     chuva = np.reshape(chuva,(157,-1))
-
-    # Nome de shape para obter os estados brasileiros.
-    # São necessários os arquivos *.shp, *.shx e *.dbf
-    shapeFile = 'shapes/BRA_adm1.shp'
-
+    
     # Nome do arquivo com parâmetros do mapa.
     mapTemplate = 'templates/ChuvaPrevistaONS.dat'
 
     # Título do mapa
     titulo_mapa = 'Precipitação (mm)\nTeste'
+        
+    # Utilize a classe 'ArquivoShape' para criar uma instância de um arquivo shape com uma cor e uma 
+    # espessura da linha especificados.
+    # Para utilizar arquivos tipo 'shape', são necessários os arquivos '*.shp', '*.shx' e '*.dbf'.
+    # Repare que você ainda poderá utilizar como 'shapeFile' apenas uma 'string' ou um objeto 'ArquivoShape'.
+
+    # Criando um objeto 'ArquivoShape' com os Estados do Brasil, cor da linha cinza e espessura da linha 1.0.
+    shapeEstadosBrasil = plotMap.ArquivoShape(nomeArquivo='shapes/BRA_adm1.shp', corLinha='gray', espLinha=1.0)
+    
+    # Criando um objeto 'ArquivoShape' com das bacias hidrográficas , cor da linha azul e espessura da linha 0.25.
+    shapeBaciasBrasil =  plotMap.ArquivoShape(nomeArquivo='shapes/Bacias.shp', corLinha='blue', espLinha=0.25)    
+    
+    # Lista contendo os dois 'ArquivosShape' acima. Ao passar uma lista de shapes, a rotina 'plotarMapa' irá adicionar
+    # cada arquivo ao mapa na mesma ordem da lista.
+    listaShapes = [shapeEstadosBrasil, shapeBaciasBrasil]
 
 
-    # Na versão 0.11 foi introduzida a classe 'ArquivoShape', para permitir que o 'shapeFile' seja reaproveitado
-    # com mínimo overhead. Você ainda pode utilizar como argumento da função 'plotarMapa' uma string,
-    
-    meuArquivoShape = plotMap.ArquivoShape(shapeFile)
-    
     # Exemplo 1: Usando o modelo de mapa de forma integral.
-    #            Neste caso:
-    #                        1) o mapa utilizará todos os parâmetros do arquivo especificado em 'mapTemplate';
-    #                        2) O quinto argumento do método 'plotarMapa' corresponde ao nome do arquivo contendo 
-    #                        os parâmetros do modelo de mapa.               
-
-    # plotMap.plotarMapa(titulo_mapa, lons,lats,chuva,mapTemplate, 'exemplo1_mapaTipoONS.jpg',meuArquivoShape)
-
-    # Se preferir, pode utilizar a versão com parâmetros nomeados da função
-    plotMap.plotarMapa(
-                        titulo=titulo_mapa,
+    # Neste caso o mapa utilizará todos os parâmetros do arquivo especificado em 'mapTemplate'.
+    # Também usaremos os 'shapes' dos Estados e das bacias.         
+    plotMap.plotarMapa( titulo=titulo_mapa,
                         lons=lons,
                         lats=lats, 
                         dados=chuva,
                         modeloMapa=mapTemplate,                       
                         destino='exemplo1_mapaTipoONS.jpg',
-                        shapeFile=meuArquivoShape
+                        shapeFile=listaShapes                   
                         )
 
 
     # Exemplo 2: Usando o modelo de mapa de forma parcial.
-    #            Neste exemplo:
-    #                           1)Vamos ler os parâmetros do mapa modelo para o objeto 'Mapa', alterar alguns
-    #                           parâmetros e plotar os mesmos dados de chuva;
-    #                           2) O quinto argumento do método 'plotarMapa' corresponde ao objeto 'Mapa' que
-    #                           teve seus parâmetros alterados.
-
-    # O método 'loadMapTemplate' recebe o nome do arquivo de modelo do mapa e retorna um objeto 'Mapa'.    
+    # Neste exemplo vamos ler os parâmetros do mapa modelo para o objeto 'Mapa' e alterar alguns.
+    # O método 'loadMapTemplate' recebe o nome do arquivo de modelo do mapa e retorna um objeto 'Mapa'. 
+    # Usaremos somente o 'shape' dos Estados.   
     meuMapaCustomizado = plotMap.loadMapTemplate(mapTemplate)
 
-    # Com o objeto 'Mapa', podemos alterar quaisquer dos parâmetros do objeto.
+    # Com o objeto 'Mapa', podemos alterar quaisquer dos parâmetros.
     meuMapaCustomizado.barraCores_titulo = "barra de cores alterada!"
     meuMapaCustomizado.barraCores_orientacao = 'vertical'
     meuMapaCustomizado.barraCores_posicao = 'right' 
     meuMapaCustomizado.barraCores_dist = 0.05
-
-    plotMap.plotarMapa(titulo_mapa, lons,lats,chuva,meuMapaCustomizado, 'exemplo2_MapaCustomizado.jpg',meuArquivoShape)
+    plotMap.plotarMapa(titulo_mapa, lons,lats,chuva,meuMapaCustomizado, 'exemplo2_MapaCustomizado.jpg',listaShapes[0])
 
 
     # Exemplo 3: Repetindo o exemplo 2, com mapa na tela.
-    #            Para isso, basta não informar nada como 'destino' (sexto argumento) do método 'plotarMapa'.
-    #            Na versão 0.11 foi introduzida a classe 'ArquivoShape', para permitir que o 'shapeFile' seja reaproveitado
-    #            com mínimo overhead.
-    #            Importante: como o parâmetro 'destino' não foi definido, é mandatório nomear o parâmetro posterior a ele na
-    #            lista de argumentos (neste caso 'shapeFile').
-
-    plotMap.plotarMapa(titulo_mapa, lons,lats,chuva,meuMapaCustomizado, shapeFile=meuArquivoShape)
+    # Para isso, basta não informar nada como 'destino' (sexto argumento) do método 'plotarMapa'.
+    # Importante: como o parâmetro 'destino' não foi definido, é mandatório nomear o parâmetro posterior a ele na
+    # lista de argumentos (neste caso 'shapeFile').
+    # Usaremos somente o 'shape' das bacias.
+    plotMap.plotarMapa(titulo_mapa, lons,lats,chuva, meuMapaCustomizado, shapeFile=listaShapes[1])
 
 
     # Exemplo 4: Repetindo o exemplo 2, com mapa do tipo 'anomalias' do site WXMaps.
-    # O template foi introduzido na versão 0.11.
-    # Neste exemplo, não será utilizado 'shapefile'.
-   
-    plotMap.plotarMapa(
-                        titulo='Anomalia (mm)\nWXMaps',
+    # Neste exemplo, não será utilizado 'shapefile'.   
+    plotMap.plotarMapa( titulo='Anomalia (mm)\nWXMaps',
                         lons=lons,
                         lats=lats,
                         dados=chuva,
